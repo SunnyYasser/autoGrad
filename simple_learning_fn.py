d11c757f0135415776cb6aca18ba23c8c605e787
SunnyYasser <sunnyyasser@gmail.com>
2020-06-18 17:51:33 +0530

    minor fixes and tests added

--- simple_learning_fn.py
+++ simple_learning_fn.py
@@ -1,17 +1,27 @@
 import numpy as np
 from autograd.tensor import Tensor
+from autograd.parameters import Parameter
+from autograd.module import Module
+from autograd.optim import SGD
 
 x_data = Tensor(np.random.randn(100,3))
 coeff = Tensor(np.array([-1., 3., -2.]))
-
 y_data = x_data@coeff + 5 
-print(y_data)
 
-w = Tensor(np.random.randn(3), requires_grad=True)
-b = Tensor(np.random.randn(), requires_grad=True)
+class Model(Module):
+    def __init__(self) -> None:
+        self.w  = Parameter(3)
+        self.b  = Parameter()
 
+    def predict(self, x:Tensor) -> Tensor:
+        return x @ self.w + self.b
+
+
+model = Model()    
 lr = 0.001
 batch_size = 32
+optimizer = SGD(lr)
+
 
 for epoch in range(100):
     
@@ -22,20 +32,18 @@ for epoch in range(100):
         
         end = start + batch_size
         inputs = x_data[start:end]
-        
-        w.zero_grad()
-        b.zero_grad()
+        model.zero_grad()
 
-        preds = inputs @ w + b
+        preds = model.predict(inputs)
         actuals = y_data[start:end]
         
         err = preds - actuals
         loss = (err*err).sum()
 
         loss.backward()
+        epoch_loss += loss
 
-        w -= w.grad * lr
-        b -= b.grad * lr
+        optimizer.step(model)
 
-    print(epoch, loss)
+    print(epoch, epoch_loss)
 
--- tests/test_tensor_matul.py
+++ tests/test_tensor_matul.py
@@ -20,6 +20,6 @@ class TestTensorMatMul(unittest.TestCase):
         grad = Tensor([[-1], [-2], [-3]])
         t3.backward(grad)
 
-        np.testing.assert_array_equal(t1.grad.data,grad.data @ t2.data.T)
+        np.testing.assert_array_equal(t1.grad.data, grad.data @ t2.data.T)
 
         np.testing.assert_array_equal(t2.grad.data, t1.data.T @ grad.data)
